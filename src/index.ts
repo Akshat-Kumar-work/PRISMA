0d44a16db3c82ca104125b2baba9b3f48c7e8123
Akshat <iamakku0.0.0.1@gmail.com>
2024-03-08 21:31:38 +0530

    update users function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+//to create table
 async function insertUser(email:string,password:string,firstName:string,lastName:string){
-   const response =  prisma.user.create({
+   const response = await prisma.users.create({
         data:{
             email,firstName,lastName,password
+        },
+        select:{
+            id:true,
+            firstName:true,
         }
+ 
     })
     console.log(response)
+};
+
+//insertUser("akshat1@gmail.com","coolCoder","akshat","dhoundiyal");
+
+interface UpdatePrams{
+    firstName?:string;
+    lastName?:string;
 }
+
+const updateParamas={
+    firstName:"cool",
+    lastName:"coder"
+}
+
+//to update table
+async function updateUsers(email:string,updateParamas:UpdatePrams) {
+    const result  = await prisma.users.update({
+        where:{email:email},
+        data:{
+            firstName:updateParamas.firstName,
+            lastName:updateParamas.lastName
+        }
+    })
+    console.log(result);
+}
+
+updateUsers("akshat1@gmail.com",updateParamas);
